4648e751d28a2caa7f289471a237785082c4ec01
Rishi <choukseyrishi89@gmail.com>
2025-08-22 12:20:13 +0530

    dotenv

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// Backend base URL
-export const base_URL = "https://feedback-collector-backend-jkt3.onrender.com";
-
 // Axios instance
 export const clientServer = axios.create({
-  baseURL: base_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Backend base URL
 });
